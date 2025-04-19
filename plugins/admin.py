4bc29087629f0fd125a03c31277f38983cb73029
siam420borna <siam420borna@gmail.com>
2025-04-19 17:25:36 +0600

    Add files via upload

---
+++ plugins/admin.py
@@ -0,0 +1,5 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from helper.database import ban_user, total_user  # removed remove_ban
+
+# Further code using remove_ban must be commented or removed
